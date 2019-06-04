3cd0208dc6df5a499aa4d502b6446fecc6c361a1
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-06-03 23:43:49 -0300

    Hasta video 20 zulu

---
+++ Models/MensajesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Chat.Data.Entities;
+namespace Chat.Models
+{
+
+    using Microsoft.AspNetCore.Http;
+    using System.ComponentModel.DataAnnotations;
+
+    public class MensajesViewModel : Mensajes
+    {
+        [Display(Name = "Adjunto")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
